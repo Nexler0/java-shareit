9b6fd40f07695fe3d847b58beaad2cf7f2891553
Nexler <interestFor@yandex.ru>
2022-08-27 20:29:56 +0300

    15 Sprint
    feat : write tests

--- src/main/java/ru/practicum/shareit/comment/model/Comment.java
+++ src/main/java/ru/practicum/shareit/comment/model/Comment.java
@@ -10,6 +10,7 @@ import ru.practicum.shareit.user.model.User;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
+import java.util.Objects;
 
 @Entity
 @Table(name = "comments")
@@ -35,4 +36,17 @@ public class Comment {
     @OneToOne
     @JoinColumn(name = "author_id")
     private User author;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Comment comment = (Comment) o;
+        return Objects.equals(text, comment.text) && Objects.equals(item, comment.item) && Objects.equals(author, comment.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(text, item, author);
+    }
 }
